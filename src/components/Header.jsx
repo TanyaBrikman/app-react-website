513856b2efaf2b796843eaeadf565651ec5497f5
Tanya <tanyabrikman@mail.ru>
2023-10-09 11:18:50 +0700

    start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://png.pngtree.com/png-vector/20230421/ourmid/pngtree-outdoor-camping-logo-vector-image_6718364.png"
+                alt=""/>
+        </header>
+    )
+
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://phonoteka.org/uploads/posts/2022-09/1663693457_4-phonoteka-org-p-fon-dlya-mangaliba-oboi-8.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava+ description
+            </div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+export default Profile
